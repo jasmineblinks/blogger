99d9aa4fd246932adbe53ff5e95ede586711c4a0
jasmineblinks <puenehfaith@gmail.com>
2021-03-16 14:51:24 +0100

    updated the header

--- src/comp/Header/Header.js
+++ src/comp/Header/Header.js
@@ -1,44 +1,58 @@
 import { useContext } from "react";
-import "./Header.css"
-import {StateContext} from "../../App"
-import {motion}from 'framer-motion';
+import "./Header.css";
+import { StateContext } from "../../App";
+import { motion } from "framer-motion";
 
 // import { useCallback, useEffect,useState } from "react";
 import HeaderNavigation from "./HeaderNavigation";
 import HeaderNavigationMobile from "./HeaderNavigationMobile";
 const Header = () => {
+  const { isMobileMenuOpen, isMobileVersion, setIsMobileMenuOpen } = useContext(
+    StateContext
+  );
 
-    const {isMobileMenuOpen, isMobileVersion, setIsMobileMenuOpen} = useContext(StateContext)
+  return (
+    <header className="header">
+      {isMobileVersion ? (
+        <HeaderNavigationMobile
+          isOpen={isMobileMenuOpen}
+          setIsOpen={setIsMobileMenuOpen}
+        />
+      ) : (
+        <HeaderNavigation />
+      )}
 
-    return ( 
-        <header className="header">
-            {isMobileVersion ? 
-            <HeaderNavigationMobile isOpen={isMobileMenuOpen} setIsOpen={setIsMobileMenuOpen} /> 
-            : <HeaderNavigation/>}
+      {/* IF MOBILE MENU IS OPEN HEADER TEXT DISAPPEAR */}
+      {!isMobileMenuOpen && (
+        <div className="headerText">
+          <motion.h1
+            initial={{ y: -250 }}
+            animate={{ y: -10 }}
+            transition={{ dealy: 1.5, duration: 2 }}>
+            A modern publishing platform
+          </motion.h1>
+          <span>Grow your audience and build online board</span>
+          <div className="headerText_buttonsContainer">
+            <motion.div
+              className="button startButton"
+              initial={{ x: "-100vw" }}
+              animate={{ x: 0 }}
+              transition={{ delay: 1.5, duration: 2 }}>
+              Start for Free
+            </motion.div>
+            <motion.div
+              className="button learnButton"
+              initial={{ x: "100vw" }}
+              animate={{ x: 0 }}
+              transition={{ delay: 1.5, duration: 2 }}>
+              Learn More
+            </motion.div>
+          </div>
+          <div></div>
+        </div>
+      )}
+    </header>
+  );
+};
 
-
-            {/* IF MOBILE MENU IS OPEN HEADER TEXT DISAPPEAR */}
-            {!isMobileMenuOpen && 
-            <div className="headerText">
-                <motion.h1 animate={{
-      scale: [1, 2, 2, 1, 1],
-      rotate: [0, 0, 270, 270, 0],
-      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-    }}>A modern publishing platform</motion.h1>
-                <span>Grow your audience and build online board</span>
-                <div className="headerText_buttonsContainer">
-                    <div className="button startButton">Start for Free</div>
-                <div className="button learnButton">Learn More</div>
-                </div>
-                <div></div>
-                
-            </div>
-            } 
-            
-            
-
-        </header>
-     );
-}
- 
 export default Header;
